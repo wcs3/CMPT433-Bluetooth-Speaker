19be1f169308546943d544b4bf2a1599e3f4bfce
Arvin <arvin.bm@gmail.com>
2025-03-27 21:37:36 -0700

    Add audio_capture.c/.h which uses ALSA to get raw input from the microphone

---
+++ bluetooth-speaker-app/hal/include/hal/audio_capture.h
@@ -0,0 +1,41 @@
+#ifndef _AUDIO_CAPTURE_H_
+#define _AUDIO_CAPTURE_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#define AUDIO_BUFFER_SIZE 4096
+
+/*
+Explanation of each function provided by this module:
+1. audio_capture_init:
+    Initializes the ALSA input stream which is the microphone
+
+2. audio_capture_cleanup:
+    Frees any allocated resources
+
+3. audio_capture_start:
+    Starts a background thread that captures the audio using the
+    microphone and fills a buffer with the captures data.
+    The captured data is in the format:
+        - Signed 16-bit little-endian PCM samples
+        - Mono audio
+        - 16000 Hz
+    NOTE: This format is compatible with the VOSK libarary used in microphone.c
+    to translate this data into text.
+
+4. audio_capture_stop
+    Stops the background thread
+
+5. audio_capture_get_audio_buffer
+    Copies the most recent audio buffer into the provided buffer &
+    returns the number of samples copied
+*/
+
+void audio_capture_init(void);
+void audio_capture_cleanup(void);
+void audio_capture_start(void);
+void audio_capture_stop(void);
+size_t audio_capture_get_audio_buffer(short* buffer);
+
+#endif
---
+++ bluetooth-speaker-app/hal/src/audio_capture.c
@@ -0,0 +1,23 @@
+#include <hal/audio_capture.h>
+#include <alsa/asoundlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+void audio_capture_init(void) {
+    // TODO
+}
+
+void audio_capture_cleanup(void) {
+    // TODO
+}
+void audio_capture_start(void) {
+    // TODO
+}
+
+void audio_capture_stop(void) {
+    // TODO
+}
+
+size_t audio_capture_get_audio_buffer(short* buffer) {
+    // TODO
+}
