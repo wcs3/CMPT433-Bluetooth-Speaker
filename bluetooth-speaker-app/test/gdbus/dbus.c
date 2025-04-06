8111ff399c3337139fd212ac62139984d2da1ea7
Dmitrii B <dmitry.a.beliaev@gmail.com>
2025-04-05 20:44:28 -0700

    Added parsing of values from proxy. And basic send of commands

--- bluetooth-speaker-app/test/gdbus/dbus.c
+++ bluetooth-speaker-app/test/gdbus/dbus.c
@@ -9,6 +9,9 @@
 #include <glib.h>
 #include <gio/gio.h>
 
+#include "player.h"
+
+
 #define BLUEZ_MEDIA_PLAYER_INTERFACE "org.bluez.MediaPlayer1"
 #define BLUEZ_NAME "org.bluez"
 #define BLUEZ_OBJECT_MANAGER_PATH "/"
@@ -27,6 +30,183 @@ static GMainLoop *main_loop;
 static GDBusConnection *dbus_conn;
 static GDBusProxy *default_player;
 static GSList *players = NULL;
+
+
+///////////////////////// PARSING ///////////////////
+
+#include <string.h>
+
+#define MAX_STRING_LENGTH 256
+
+char current_artist[MAX_STRING_LENGTH];
+char current_title[MAX_STRING_LENGTH];
+char current_album[MAX_STRING_LENGTH];
+char current_track_status[MAX_STRING_LENGTH];
+
+void print_track_data(const gchar *str) {
+    gchar *title = NULL;
+    gchar *track_number = NULL;
+    gchar *num_tracks = NULL;
+    gchar *duration = NULL;
+    gchar *album = NULL;
+    gchar *artist = NULL;
+    gchar *status = NULL;
+
+    // Parsing 'Title'
+    gchar *title_start = strstr(str, "'Title': <'");
+    if (title_start != NULL) {
+        title_start += 11;  // Skip "'Title': <'"
+        gchar *title_end = strstr(title_start, "'>");
+        if (title_end != NULL) {
+            gint title_length = title_end - title_start;
+            title = g_strndup(title_start, title_length);
+
+            if (title_length < MAX_STRING_LENGTH) {
+                strncpy(current_title, title_start, title_length);
+                current_title[title_length] = '\0';
+            } else {
+                // If string is too long, just copy the maximum allowed length
+                strncpy(current_title, title_start, MAX_STRING_LENGTH - 1);
+                current_title[MAX_STRING_LENGTH - 1] = '\0';
+            }
+        }
+    }
+
+    // Parsing 'TrackNumber'
+    gchar *track_num_start = strstr(str, "'TrackNumber': <");
+    if (track_num_start != NULL) {
+        track_num_start += 23;
+        gchar *track_num_end = strstr(track_num_start, ">");
+        if (track_num_end != NULL) {
+            gint track_num_length = track_num_end - track_num_start;
+            track_number = g_strndup(track_num_start, track_num_length);
+        }
+    }
+    int tarck_num_test = (int) g_ascii_strtoull(track_number, NULL, 10);
+
+    // Parsing 'NumberOfTracks'
+    gchar *num_tracks_start = strstr(str, "'NumberOfTracks': <");
+    if (num_tracks_start != NULL) {
+        num_tracks_start += 26;
+        gchar *num_tracks_end = strstr(num_tracks_start, ">");
+        if (num_tracks_end != NULL) {
+            gint num_tracks_length = num_tracks_end - num_tracks_start;
+            num_tracks = g_strndup(num_tracks_start, num_tracks_length);
+        }
+    }
+    int num_tracks_test = (int) g_ascii_strtoull(num_tracks, NULL, 10);
+
+    // Parsing 'Duration'
+    gchar *duration_start = strstr(str, "'Duration': <uint32 ");
+    if (duration_start != NULL) {
+        duration_start += 19; 
+        gchar *duration_end = strstr(duration_start, ">");
+        if (duration_end != NULL) {
+            gint duration_length = duration_end - duration_start;
+            duration = g_strndup(duration_start, duration_length);
+        }
+    }
+    int dur_test = (int) g_ascii_strtoull(duration, NULL, 10);
+
+    // Parsing 'Album'
+    gchar *album_start = strstr(str, "'Album': <'");
+    if (album_start != NULL) {
+        album_start += 11;
+        gchar *album_end = strstr(album_start, "'>");
+        if (album_end != NULL) {
+            gint album_length = album_end - album_start;
+            album = g_strndup(album_start, album_length);
+
+            if (album_length < MAX_STRING_LENGTH) {
+                strncpy(current_album, album_start, album_length);
+                current_album[album_length] = '\0';
+            } else {
+                strncpy(current_album, album_start, MAX_STRING_LENGTH - 1);
+                current_album[MAX_STRING_LENGTH - 1] = '\0';
+            }
+        }
+    }
+
+    // Parsing 'Artist'
+    gchar *artist_start = strstr(str, "'Artist': <'");
+    if (artist_start != NULL) {
+        artist_start += 12;
+        gchar *artist_end = strstr(artist_start, "'>");
+        if (artist_end != NULL) {
+            gint artist_length = artist_end - artist_start;
+            artist = g_strndup(artist_start, artist_length);
+
+            if (artist_length < MAX_STRING_LENGTH) {
+                strncpy(current_artist, artist_start, artist_length);
+                current_artist[artist_length] = '\0';
+            } else {
+                strncpy(current_artist, artist_start, MAX_STRING_LENGTH - 1);
+                current_artist[MAX_STRING_LENGTH - 1] = '\0';
+            }
+        }
+    }
+
+    GVariant *property = g_dbus_proxy_get_cached_property(default_player, "Status");
+    gchar *str_other = g_variant_print(property, FALSE);
+
+    gchar *status_start = strstr(str_other, "'");
+    if (status_start != NULL) {
+        status_start += 1;
+        gchar *status_end = strstr(status_start, "'");
+        if (status_end != NULL) {
+            gint status_length = status_end - status_start;
+            status = g_strndup(status_start, status_length);
+
+            if (status_length < MAX_STRING_LENGTH) {
+                strncpy(current_track_status, status_start, status_length);
+                current_track_status[status_length] = '\0';
+            } else {
+                strncpy(current_track_status, status_start, MAX_STRING_LENGTH - 1);
+                current_track_status[MAX_STRING_LENGTH - 1] = '\0';
+            }
+        }
+    }
+
+    property = g_dbus_proxy_get_cached_property(default_player, "Position");
+    str_other = g_variant_print(property, FALSE);
+    int curr_pos_test = (int) g_ascii_strtoull(str_other, NULL, 10);
+
+    
+    if (current_track_status[0] != '\0') {
+        printf("Status: '%s'\n", current_track_status);
+    } else {
+        printf("Artist: Unknown\n"); }
+    if (current_title[0] != '\0') {
+        printf("Artist: '%s'\n", current_title);
+    } else {
+        printf("Artist: Unknown\n"); }
+
+    printf("Track Number: %d\n", tarck_num_test);
+    printf("Number of Tracks: %d\n", num_tracks_test);
+    printf("Duration: %d\n", dur_test ? dur_test : -1);
+    printf("Current Position: %d\n", curr_pos_test ? curr_pos_test : -1);
+
+    if (current_album[0] != '\0') {
+        printf("Album: '%s'\n", current_album);
+    } else {
+        printf("Album: Unknown\n"); }
+    if (current_artist[0] != '\0') {
+        printf("Artist: '%s'\n", current_artist);
+    } else {
+        printf("Artist: Unknown\n"); }
+    
+    // Clean up memory
+    g_free(title);
+    g_free(track_number);
+    g_free(num_tracks);
+    g_free(duration);
+    g_free(album);
+    g_free(artist);
+    g_free(status);
+    g_free(str_other);
+}
+
+/////////////////////////
 
 static void on_notify_name_owner(GObject *object, GParamSpec *pspec, gpointer user_data)
 {
@@ -78,7 +258,7 @@ void cmd_list()
         GDBusProxy *proxy = l->data;
         print_player(proxy, NULL);
     }
-}
+}   
 
 static void print_property(GDBusProxy *proxy, const char *name)
 {
@@ -213,6 +393,13 @@ static void load_existing_proxies(GDBusObjectManager *manager)
         {
             GDBusProxy *proxy = G_DBUS_PROXY(ll->data);
 
+            const gchar *iface_name = g_dbus_proxy_get_interface_name(proxy);
+
+            // g_print("\t'%s'\n", iface_name);
+
+            if (g_str_equal(iface_name, BLUEZ_MEDIA_PLAYER_INTERFACE))
+                player_added(proxy);
+
             for (GList *lll = proxy_watchers; lll; lll = lll->next)
             {
                 proxy_watch_t *watch = lll->data;
@@ -278,6 +465,78 @@ int main()
     g_free(name_owner);
 
     load_existing_proxies(manager);
+
+    // cmd_show();
+
+
+    ////////////////////////////// testing //////////////////
+
+    GVariant *property = g_dbus_proxy_get_cached_property(default_player, "Track");
+    gchar *str = g_variant_print(property, FALSE);
+
+    print_track_data(str);
+
+    
+
+    if(strcmp(current_track_status, "playing") == 0){
+        printf("\nMusic is on!\n");
+        // bt_player_send_command(BT_PLAYER_CMD_PAUSE);
+        GError *error = NULL;
+
+        if (g_dbus_proxy_call_sync(default_player,
+                                "Pause",
+                                NULL,
+                                G_DBUS_CALL_FLAGS_NONE,
+                                -1,
+                                NULL,
+                                &error) == NULL)
+        {
+            g_printerr("%s\n", error->message);
+            // return false;
+        } else {
+            printf("\nSuccess pause\n");
+        }
+    } else if(strcmp(current_track_status, "paused") == 0){
+        printf("\nMusic is off!\n");
+        // bt_player_send_command(BT_PLAYER_CMD_PLAY);
+        GError *error = NULL;
+
+        if (g_dbus_proxy_call_sync(default_player,
+                                    "Play",
+                                    NULL,
+                                    G_DBUS_CALL_FLAGS_NONE,
+                                    -1,
+                                    NULL,
+                                    &error) == NULL)
+        {
+        g_printerr("%s\n", error->message);
+        // return false;
+        } else {
+        printf("\nSuccess play\n");
+        }
+    }
+
+    error = NULL;
+
+    if (g_dbus_proxy_call_sync(default_player,
+                                "Next",
+                                NULL,
+                                G_DBUS_CALL_FLAGS_NONE,
+                                -1,
+                                NULL,
+                                &error) == NULL)
+        {
+        g_printerr("%s\n", error->message);
+        // return false;
+    } else {
+        printf("\nSuccess Next\n");
+    }
+
+    g_free(str);
+    printf("\nthe end\n");
+
+    ////////////////////////////// testing END //////////////////
+
 
     g_signal_connect(manager,
                      "notify::name-owner",
