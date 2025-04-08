62fce0cafbc2f21a0997375df4a8ce9b2d20c876
William Schmidt <iam.william.schmidt@gmail.com>
2025-04-08 23:37:12 +0000

    fix toolchain file to find arm64 libasound

--- bluetooth-speaker-app/aarch64-linux-gnu.cmake
+++ bluetooth-speaker-app/aarch64-linux-gnu.cmake
@@ -1,5 +1,6 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER   /usr/bin/aarch64-linux-gnu-gcc)
@@ -18,5 +19,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(PKG_CONFIG_EXECUTABLE aarch64-linux-gnu-pkg-config)
 
 # search headers and libraries in the target environment
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
